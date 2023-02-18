e4be70cb141e3c73a636173205c279107732f3c0
Mikhail <91342252+Mishachkld@users.noreply.github.com>
2023-02-18 10:31:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
 #define SIZE 999
+template<typename T>
+void ownMemcopy(T dst, T src, const int &len) {
+	int i, m;
+	int byteSizeArray = sizeof(T) * SIZE;
+	std::cout << sizeof(T);
+	unsigned long long *wdst = (unsigned long long *)dst;   // текущая позиция в буфере назначения (т.е. на каком байте мы находимся в новом массиве)
+	unsigned long long *wsrc = (unsigned long long *)src;   // текущая позиция в источнике(т.е. на каком байте мы находимся в старом массиве)
+	char  *pointerDst, *pointerSrc;
 
-void ownMemcopy(int *dst, int *src, const int &len) {
-    int i, m;
-    unsigned long long *wdst = (unsigned long long *)dst;   // текущая позиция в буфере назначения (т.е. на каком байте мы находимся в новом массиве)
-    unsigned long long *wsrc = (unsigned long long *)src;   // текущая позиция в источнике(т.е. на каком байте мы находимся в старом массиве)
-    char  *pointerDst, *pointerSrc;
+	m = (byteSizeArray) / sizeof(long long);          // получаем целое количество байт, которое нужно перенести ( длину нашего массива в байтах / 8 байт (т.е. long long)
+	for (i = 0; i < m; i++)                                  // копируем основную часть блоками по 8 байт
+		*(wdst++) = *(wsrc++);                              // (в зависимости от платформы)
 
-    m = (SIZE * sizeof(int))/ sizeof(long long);          // получаем целое количество байт, которое нужно перенести ( длину нашего массива в байтах / 8 байт (т.е. long long)
-    for(i = 0; i < m; i++)                                  // копируем основную часть блоками по 8 байт
-        *(wdst++) = *(wsrc++);                              // (в зависимости от платформы)
+	pointerDst = (char*)wdst;
+	pointerSrc = (char*)wsrc;
 
-    pointerDst = (char*)wdst;
-    pointerSrc = (char*)wsrc;
-
-    m = (SIZE * sizeof(int)) % sizeof(long long);          // получаем колличество оставшихся байт, которое нужно перенести ( длину нашего массива в байтах % 8 байт (делим и смотрим остаток)
-    for(i = 0; i < m; i++)                                  // остаток копируем побайтно (аналогично предыдущему)
-        *(pointerDst++) = *(pointerSrc++);
+	m = (byteSizeArray) % sizeof(long long);          // получаем колличество оставшихся байт, которое нужно перенести ( длину нашего массива в байтах % 8 байт (делим и смотрим остаток)
+	for (i = 0; i < m; i++)                                  // остаток копируем побайтно (аналогично предыдущему)
+		*(pointerDst++) = *(pointerSrc++);
 }
 
-void outArray(int *array, const int &len) {
-    for (int i = 0; i < len; i++)
-        std::cout << array[i] << " ";
-    std::cout << std::endl;
+template<typename T>
+void outArray(T array[SIZE], const int &len) {
+	for (int i = 0; i < len; i++)
+		std::cout << array[i] << " ";
+	std::cout << std::endl;
 }
 
 int main() {
-    int tecArray[SIZE] = {123, 132, 154145, 2556516, 45454,6};
-    int newArray[SIZE] = {0};
-    int len = 9;
+	std::string tecArray[SIZE] = { "123", "132", "154145", "2556516", "45454","6" };
+	std::string newArray[SIZE] = { "" };
+	int len = 6;
 
-    ownMemcopy(newArray, tecArray, len);
-    outArray(newArray, len);
-    return 0;
+	//ownMemcopy(newArray, tecArray, len);
+	//outArray(newArray, len);
+	outArray(tecArray, len);
+	system("pause");
+	return 0;
 }
-
-
-
-
-
